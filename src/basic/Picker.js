a7f691f2bf7ef3b46d01b63861ec8f5a5c4ddac8
timothydillan <timothi.dillan10@gmail.com>
2021-06-06 18:42:54 +0800

    Add picker enhancement
    This commit adds an enhancement to the picker feature that allows users to create placeholders. Refers to this issue: github.com/GeekyAnts/NativeBase/issues/3642

--- src/basic/Picker.js
+++ src/basic/Picker.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/prefer-stateless-function */
 /* eslint-disable react/prefer-es6-class */
 import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
 import createReactClass from 'create-react-class';
 import { Picker } from '@react-native-picker/picker';
 import { connectStyle } from 'native-base-shoutem-theme';
@@ -8,9 +9,18 @@ import { connectStyle } from 'native-base-shoutem-theme';
 import mapPropsToStyleNames from '../utils/mapPropsToStyleNames';
 
 export default class PickerNB extends Component {
+  constructor(props) {
+    super(props);
+    // Receives placeholder-related attributes defined by the user, if any.
+    this.placeHolderText = typeof props.placeHolderText === 'string' ? props.placeHolderText : null;
+    this.placeHolderStyle = typeof props.placeHolderStyle === 'object' ? props.placeHolderStyle : null;
+  }
+
   render() {
+    const placeHolderText = this.placeHolderText;
     return (
       <Picker ref={c => (this._root = c)} {...this.props}>
+        {placeHolderText !== null && <Picker.Item value='' label={placeHolderText} style={pickerStyles.placeHolderStyle} />}
         {this.props.children}
       </Picker>
     );
@@ -33,5 +43,12 @@ const StyledPickerNB = connectStyle(
   {},
   mapPropsToStyleNames
 )(PickerNB);
+
+const pickerStyles = StyleSheet.create({
+  placeHolderStyle: this.placeHolderStyle ? this.placeHolderStyle
+    : {
+      color: "#c8c8cb"
+    }
+});
 
 export { StyledPickerNB as PickerNB };
