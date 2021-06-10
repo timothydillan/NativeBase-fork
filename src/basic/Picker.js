b043845f71dd7e98fa461481154fbf232d232b5a
timothydillan <timothi.dillan10@gmail.com>
2021-06-10 09:13:51 +0800

    Revert picker component

--- src/basic/Picker.js
+++ src/basic/Picker.js
@@ -1,7 +1,6 @@
 /* eslint-disable react/prefer-stateless-function */
 /* eslint-disable react/prefer-es6-class */
 import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
 import createReactClass from 'create-react-class';
 import { Picker } from '@react-native-picker/picker';
 import { connectStyle } from 'native-base-shoutem-theme';
@@ -9,18 +8,9 @@ import { connectStyle } from 'native-base-shoutem-theme';
 import mapPropsToStyleNames from '../utils/mapPropsToStyleNames';
 
 export default class PickerNB extends Component {
-  constructor(props) {
-    super(props);
-    // Receives placeholder-related attributes defined by the user, if any.
-    this.placeHolderText = typeof props.placeHolderText === 'string' ? props.placeHolderText : null;
-    this.placeHolderStyle = typeof props.placeHolderStyle === 'object' ? props.placeHolderStyle : null;
-  }
-
   render() {
-    const placeHolderText = this.placeHolderText;
     return (
       <Picker ref={c => (this._root = c)} {...this.props}>
-        {placeHolderText !== null && <Picker.Item value='' label={placeHolderText} style={pickerStyles.placeHolderStyle} />}
         {this.props.children}
       </Picker>
     );
@@ -43,12 +33,5 @@ const StyledPickerNB = connectStyle(
   {},
   mapPropsToStyleNames
 )(PickerNB);
-
-const pickerStyles = StyleSheet.create({
-  placeHolderStyle: this.placeHolderStyle ? this.placeHolderStyle
-    : {
-      color: "#c8c8cb"
-    }
-});
 
 export { StyledPickerNB as PickerNB };
